2c5ee869edbe537f824f6ce68ab07629f5adaa85
rey-arx <raihan786arx@gmail.com>
2023-11-11 13:24:51 +0530

    gitignore

--- src/NodeLearn/server3.js
+++ src/NodeLearn/server3.js
@@ -3,7 +3,20 @@ const http = require('http');
 //two arg : request and response
 //res contains the where it comes , what it wants
 const server = http.createServer((req,res) => {
-    console.log("req made");
+    // console.log(req);
+    console.log(req.url);//show which url it is requesting
+    console.log(req.method); //show which method
+    //set header content type: (text type);
+    // res.setHeader('Content-Type' , 'text/plain');
+    // res.write('hello');
+    //if we need to send html
+    res.setHeader('Content-Type','text/html');
+    res.write('<p>hi</p>');
+    res.write('</br>');
+    res.write('<p>helo</p>');
+    
+    res.end();
+
 }); 
 //we have a port and a host name
 //port number are like a door into computer where diff port num for diff application
